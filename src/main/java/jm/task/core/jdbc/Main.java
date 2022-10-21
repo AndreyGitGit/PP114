9da45b5d5094749a22bcb2dd26d817cdc8038190
Andrey <gitproandrey@gmail.com>
2022-10-21 21:12:32 +0300

    Task 1.1.3.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,23 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.util.Util;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+    private static User user = new User();
+
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-//        Util util = new Util();
-//        Util.getInstance().getConnection();
+        userService.createUsersTable();
+
+        userService.saveUser("Василий", "Васильеа", (byte) 20);
+        userService.saveUser("Иван", "Иванов", (byte) 25);
+        userService.saveUser("Егор", "Егоров", (byte) 21);
+        userService.saveUser("Джон", "Уик", (byte) 41);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
